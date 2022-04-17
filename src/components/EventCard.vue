1d76de58d9721413eacea01ac9e1484420655d5c
Ralph van Kruiselbergen <git@ralphvankruiselbergen.nl>
2022-04-17 09:42:03 +0200

    my answer

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="event-card">
     <span>@{{ event.time }} on {{ event.date }}</span>
-    <h4>{{ event.title }}</h4>
+    <router-link :to="{ name: 'EventDetails', params: { id: event.id } }">
+      <h4>{{ event.title }}</h4>
+    </router-link>
   </div>
 </template>
 
---
+++ src/views/EventDetails.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>{{ event.title }}</h1>
+    <p>{{ event.title }} is on {{ event.date }} @ {{ event.location }}</p>
+  </div>
+</template>
+
+<script>
+import EventService from '@/services/EventService'
+
+export default {
+  props: {
+    id: [String, Number]
+  },
+  data() {
+    return {
+      event: {}
+    }
+  },
+  created() {
+    EventService.getEvent(this.id)
+      .then(response => {
+        this.event = response.data
+      })
+      .catch(error => {
+        alert(error)
+      })
+  }
+}
+</script>
